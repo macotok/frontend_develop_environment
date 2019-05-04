8b4892745668594dffad5e83e634b2644a9d2a52
macotok <monochromer@r8.ucom.ne.jp>
2019-05-04 18:57:32 +0900

    refactoring webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,145 +1,143 @@
-const webpack = require('webpack');
-const path = require('path');
+const Autoprefixer = require('autoprefixer');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const Autoprefixer = require('autoprefixer');
+const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = [
-  {
-    entry: {
-      app: [
-        './src/js/app.js',
-        './src/sass/style.scss',
-      ],
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/[name].js',
-      publicPath: '/',
-    },
-    devServer: {
-      contentBase: './dist/html/',
-      watchContentBase: true,
-      port: 3000,
-      open: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env'],
+module.exports = {
+  entry: {
+    app: [
+      './src/js/app.js',
+      './src/sass/style.scss',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: './dist/html/',
+    watchContentBase: true,
+    port: 3000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
             },
-            {
-              loader: 'css-loader',
-              options: {
-                url: false,
-                sourceMap: true,
-              },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                Autoprefixer(
+                  {
+                    browsers: ['last 2 versions', 'Android >= 4'],
+                  },
+                ),
+              ],
             },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  Autoprefixer(
-                    {
-                      browsers: ['last 2 versions', 'Android >= 4'],
-                    },
-                  ),
-                ],
-              },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)(\?[a-z0-9=.]+)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-            },
-          ],
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-          use: [{
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)(\?[a-z0-9=.]+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './webfonts',
+            publicPath: '../webfonts',
+          },
+        }],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: './webfonts',
-              publicPath: '../webfonts',
             },
-          }],
-        },
-        {
-          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-              },
-            },
-          ],
-        },
-      ],
-    },
-    optimization: {
-      minimizer: [
-        new OptimizeCSSAssetsPlugin(),
-        new UglifyJsPlugin(),
-      ],
-    },
-    resolve: {
-      extensions: ['.js'],
-    },
-    devtool: 'source-map',
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: path.resolve(__dirname, 'src/images/'),
-          to: path.resolve(__dirname, 'dist/images/'),
-        },
-        {
-          from: path.resolve(__dirname, 'src/webfonts/'),
-          to: path.resolve(__dirname, 'dist/webfonts/'),
-        },
-        {
-          from: path.resolve(__dirname, 'src/media/'),
-          to: path.resolve(__dirname, 'dist/media/'),
-        },
-      ]),
-      new MiniCssExtractPlugin({
-        filename: 'css/style.css',
-      }),
-      new ImageminPlugin({
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        pngquant: {
-          quality: '95-100',
-        },
-      }),
+          },
+        ],
+      },
     ],
   },
-];
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin(),
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/images/'),
+        to: path.resolve(__dirname, 'dist/images/'),
+      },
+      {
+        from: path.resolve(__dirname, 'src/webfonts/'),
+        to: path.resolve(__dirname, 'dist/webfonts/'),
+      },
+      {
+        from: path.resolve(__dirname, 'src/media/'),
+        to: path.resolve(__dirname, 'dist/media/'),
+      },
+    ]),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '95-100',
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+};
